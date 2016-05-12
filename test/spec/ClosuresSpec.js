5ccae6ee10180c374a592eacfebb6524495a474a
asBrettisay <brett.caudill.is@gmail.com>
2016-05-12 17:10:07 -0600

    Finished tests for all problems.

--- test/spec/ClosuresSpec.js
+++ test/spec/ClosuresSpec.js
@@ -51,6 +51,117 @@ describe('closures', function () {
 		})
 	})
 
+describe('counterFactory', function() {
+	it('should exist', function() {
+		expect(counterFactory).toBeDefined();
+		expect(counter).toBeDefined();
+	})
+
+	it('should be a module', function() {
+		expect(counter.inc).toEqual(jasmine.any(Function));
+		expect(counter.dec).toEqual(jasmine.any(Function));
+	})
+
+	it('should increment', function() {
+		var c = counterFactory(5);
+		expect(c.inc()).toEqual(6);
+		expect(c.inc()).toEqual(7);
+	})
+
+	it('should decrement', function() {
+		var c = counterFactory(5);
+		expect(c.dec()).toEqual(4);
+		expect(c.dec()).toEqual(3);
+	})
+})
+
+describe('motivation', function() {
+	var test = (function() {
+		var names = [
+			'Brett Caudill',
+			'Jessica Hathaway',
+			'Breiden Busch',
+			'Brian Kemper'
+		];
+
+		var thisName = names[Math.floor(Math.random() * names.length)].split(" ");
+
+		return {
+			first: thisName[0],
+			last: thisName[1]
+		}
+	})();
+
+	it('should exist', function() {
+		expect(motivation).toEqual(jasmine.any(Function))
+	})
+
+	it('should greet correctly', function() {
+		expect(motivation(test.first, test.last)).toEqual(
+			"You're doing awesome, keep it up " + test.first + " " + test.last + "."
+		)
+	})
+})
+
+describe('module', function() {
+	it('should exist', function() {
+		expect(module).toEqual(jasmine.any(Object));
+	})
+
+	it('should have a property called publicMethod', function() {
+		expect(module.publicMethod).toEqual(jasmine.any(Function));
+	})
+
+	it('should invoke publicMethod to get the privateMethod', function() {
+		expect(module.publicMethod()).toEqual('Hi, I\'m phillip, age 29 from Utah');
+	})
+})
+
+describe('timeOutCounter', function() {
+	beforeEach(function() {
+		jasmine.clock().install();
+		spyOn(console, 'log');
+	});
+
+	afterEach(function() {
+		jasmine.clock().uninstall();
+	})
+
+	it('should exist', function() {
+		expect(timeOutCounter).toEqual(jasmine.any(Function))
+	})
+
+
+	it('should call setTimeout 5 times', function() {
+
+		timeOutCounter();
+
+		jasmine.clock().tick(500);
+		expect(console.log.calls.count()).toEqual(1);
+		expect(console.log).toHaveBeenCalledWith(0);
+
+		jasmine.clock().tick(1000);
+		expect(console.log.calls.count()).toEqual(2);
+		expect(console.log).toHaveBeenCalledWith(1);
+
+		jasmine.clock().tick(1000);
+		expect(console.log.calls.count()).toEqual(3);
+		expect(console.log).toHaveBeenCalledWith(2);
+
+		jasmine.clock().tick(1000);
+		expect(console.log.calls.count()).toEqual(4);
+		expect(console.log).toHaveBeenCalledWith(3);
+
+		jasmine.clock().tick(1000);
+		expect(console.log.calls.count()).toEqual(5);
+		expect(console.log).toHaveBeenCalledWith(4);
+
+		jasmine.clock().tick(1000);
+		expect(console.log.calls.count()).toEqual(6);
+		expect(console.log).toHaveBeenCalledWith(5);
+	})
+})
+
 
 describe('funcArray', function () {
 	it('should exist', function () {
